c68f9a15413254aa2d7ed9ed35c6d4cb18eda30a
minfrank <minfrank35@gmail.com>
2023-11-13 00:22:56 +0900

    status_bar hide, navigation bar hide

--- app/src/main/java/com/inandout/real_rider/MainActivity.kt
+++ app/src/main/java/com/inandout/real_rider/MainActivity.kt
@@ -1,11 +1,61 @@
 package com.inandout.real_rider
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.MotionEvent
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import com.inandout.real_rider.util.hideSystemUI
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        hideSystemUI()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        hideSystemUI()
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        /**
+         * hide status bar code start
+         */
+        val X = event.x.toInt()
+        val Y = event.y.toInt()
+        val eventaction = event.action
+        if (Y < 400) {
+            onWindowFocusChanged(true)
+            //    Toast.makeText(this, "ACTION_DOWN AT COORDS " + "X: " + X + " Y: " + Y, Toast.LENGTH_SHORT).show();
+        }
+
+        /**
+         * hide status bar code stop
+         */
+        return true
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+
+        /**
+         * hide status bar code start
+         */
+        Log.d("Focus debug", "Focus changed !")
+        if (!hasFocus) {
+            Log.d("Focus debug", "Lost focus !")
+            val closeDialog = Intent(Intent.ACTION_CLOSE_SYSTEM_DIALOGS)
+            sendBroadcast(closeDialog)
+        }
+        /**
+         * hide status bar code stop
+         */
     }
 }
