2a5d3a056b55b3a486db314aa6a1821f0e036943
minfrank1 <minfrank@naver.com>
2023-11-13 16:10:29 +0900

    권한 삭제

--- app/src/main/java/com/inandout/real_rider/util/SystemUtil.kt
+++ app/src/main/java/com/inandout/real_rider/util/SystemUtil.kt
@@ -10,21 +10,9 @@ import com.inandout.real_rider.R
 
 fun Activity.hideSystemUI() {
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-        window.insetsController?.let {
-            // Default behavior is that if navigation bar is hidden, the system will "steal" touches
-            // and show it again upon user's touch. We just want the user to be able to show the
-            // navigation bar by swipe, touches are handled by custom code -> change system bar behavior.
-            // Alternative to deprecated SYSTEM_UI_FLAG_IMMERSIVE.
-            it.systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
-            // make navigation bar translucent (alternative to deprecated
-            // WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION)
-            // - do this already in hideSystemUI() so that the bar
-            // is translucent if user swipes it up
-            window.navigationBarColor = getColor(R.color.black)
-            // Finally, hide the system bars, alternative to View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-            // and SYSTEM_UI_FLAG_FULLSCREEN.
-            it.hide(WindowInsets.Type.systemBars())
-        }
+        window.insetsController?.systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        window.navigationBarColor = getColor(R.color.black)
+        window.insetsController?.hide(WindowInsets.Type.systemBars())
     } else {
         // Enables regular immersive mode.
         // For "lean back" mode, remove SYSTEM_UI_FLAG_IMMERSIVE.
